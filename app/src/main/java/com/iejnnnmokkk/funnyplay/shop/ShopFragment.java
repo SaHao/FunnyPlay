9a4d660570a5425a459948d80ca874746dbc473a
WhenSun <948004034@qq.com>
2024-12-10 14:30:47 +0800

    配置

---
+++ app/src/main/java/com/iejnnnmokkk/funnyplay/shop/ShopFragment.java
@@ -0,0 +1,28 @@
+package com.iejnnnmokkk.funnyplay.shop;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.iejnnnmokkk.common.base.BaseFragment;
+
+/**
+ * @author Sun
+ * @Demo class HomeFragment
+ * @Description TODO
+ * @date 2024/12/9 13:52
+ */
+public class ShopFragment extends BaseFragment {
+
+    @Override
+    protected View onInitView(@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
